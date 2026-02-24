9163ee4fbca8d145c7e7483d92e107c675ba0306
LarissaDias-Barbosa <larissaconceicaovitorinodias@gmail.com>
2026-02-24 14:48:46 -0300

    Estrutura inicial do projeto SafePass

---
+++ SafePass/src/test/java/com/dobasicoaoaavancado/SafePass/SafePassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobasicoaoaavancado.SafePass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafePassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
